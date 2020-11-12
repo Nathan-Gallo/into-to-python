2a462297bb28e736ea2a7eb4cb4fa56a25df480f
Gallo, Nathan M (HIMCO, FIN) <nathan.gallo@thehartford.com>
2020-11-12 12:06:46 -0500

    added inheiritance

--- airtravel.py
+++ airtravel.py
@@ -101,33 +101,35 @@ class Flight:
                     yield (passenger, f"{row}{letter}")
 
 
-class Airbus319:
+class Aircraft:
+
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
 
     def __init__(self, registration):
         self._registration = registration
 
     def registration(self):
         return self._registration
+
+
+class Airbus319(Aircraft):
 
     def model(self):
         return "Airbus A319"
 
     def seating_plan(self):
-        return range(1,23), "ABCDEF"
+        return range(1, 23), "ABCDEF"
 
-class Boeing777:
 
-    def __init__(self, registration):
-        self._registration = registration
-
-    def registration(self):
-        return self._registration
+class Boeing777(Aircraft):
 
     def model(self):
         return "Boeing 777"
 
     def seating_plan(self):
-        return range(1,56), "ABCDEGHJK"
+        return range(1, 56), "ABCDEGHJK"
 
 
 def make_flights():
